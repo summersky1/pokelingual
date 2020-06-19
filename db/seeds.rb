23a1099d4fd0ac3ee282bd49b0e39edd5a31898c
Ajay <ajay.summer2@gmail.com>
2020-06-19 19:09:40 +0100

    Seed data from Pokemon dataset to model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+Pokemon.delete_all
+
+CSV.foreach('lib/pokemon.csv') do |row|
+  Pokemon.create({
+    id: row[0],
+    name_english: row[1],
+    name_japanese: row[2],
+    name_romaji: row[3]
+  })
+end
+
+puts "Finished seeding Pokemon data!"
