a38c2b497161046c3c6f0ffde44c5e2bc5277bd4
Ajay <ajay.summer2@gmail.com>
2020-06-20 17:05:51 +0100

    Add Pokemon image to show view using link to external site

--- app/helpers/pokemon_helper.rb
+++ app/helpers/pokemon_helper.rb
@@ -1,2 +1,11 @@
 module PokemonHelper
+  def pokemon_large_image_url(pokemon_id)
+    formatted_id = pokemon_id.to_s.rjust(3, '0')
+    url = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/#{formatted_id}.png"
+  end
+
+  def pokemon_thumbnail_image_url(pokemon_id)
+    formatted_id = pokemon_id.to_s.rjust(3, '0')
+    url = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/#{formatted_id}.png"
+  end
 end
