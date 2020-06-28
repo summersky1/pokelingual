2083525fdff20670e1a8c412aa72a881b8a39229
Ajay <ajay.summer2@gmail.com>
2020-06-28 12:44:41 +0100

    Fix some values not seeding due to comparing string value with an int

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,7 +26,7 @@ end
 previous_id = 0
 CSV.foreach('lib/datasets/pokemon_name_origins_jp.csv') do |row|
   # ignore alternate pokemon forms etc.
-  if row[0] != previous_id
+  if row[0].to_i != previous_id
     pokemon = Pokemon.find(row[0])
     pokemon.update(name_origin_japanese: row[3], name_origin_japanese_for_english: row[4])
     previous_id = previous_id + 1
