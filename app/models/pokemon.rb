3d5cf5645edb99eca3761943ae331eba96c526b4
Ajay <ajay.summer2@gmail.com>
2020-07-03 15:26:41 +0100

    Display Pokemon region in show view

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,6 +1,7 @@
 class Pokemon < ApplicationRecord
   has_many :pokemon_types
   has_many :types, :through => :pokemon_types
+  belongs_to :generation
 
   def self.get_random_pokemon(quantity)
     Pokemon.find(Pokemon.pluck(:id).sample(quantity))
