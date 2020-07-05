e53e2613473c3fc5b7ce4807a27f0f0565e1ae67
Ajay <ajay.summer2@gmail.com>
2020-07-05 12:47:14 +0100

    Fix Pokemon in generation view not in order

--- app/models/generation.rb
+++ app/models/generation.rb
@@ -2,6 +2,6 @@ class Generation < ApplicationRecord
   has_many :pokemons
 
   def self.get_pokemon_from_generation(generation, page)
-    Generation.find(generation).pokemons.page(page).per(15)
+    Generation.find(generation).pokemons.order(:id).page(page).per(15)
   end
 end
