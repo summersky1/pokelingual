f9a60be8af04066f2167bcb19c200d46ec7cd7d9
Ajay <ajay.summer2@gmail.com>
2020-07-10 21:59:16 +0100

    Add get previous and next Pokemon model methods

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -6,4 +6,22 @@ class Pokemon < ApplicationRecord
   def self.get_random_pokemon(quantity)
     Pokemon.find(Pokemon.pluck(:id).sample(quantity))
   end
+
+  def self.get_previous_pokemon(pokemon_id)
+    if pokemon_id > 1
+      pokemon_id = pokemon_id - 1
+    else
+      pokemon_id = Pokemon.count
+    end
+    Pokemon.find(pokemon_id)
+  end
+
+  def self.get_next_pokemon(pokemon_id)
+    if pokemon_id < Pokemon.count
+      pokemon_id = pokemon_id + 1
+    else
+      pokemon_id = 1
+    end
+    Pokemon.find(pokemon_id)
+  end
 end
