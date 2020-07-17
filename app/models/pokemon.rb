1510dd4d7b19ce5e149f5dcce87762aa6b7f1153
Ajay <ajay.summer2@gmail.com>
2020-07-17 19:06:54 +0100

    Add Elasticsearch index config to Pokemon model

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,7 +1,26 @@
+require 'elasticsearch/model'
+
 class Pokemon < ApplicationRecord
   has_many :pokemon_types
   has_many :types, :through => :pokemon_types
   belongs_to :generation
+
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
+  def as_indexed_json(options = {})
+    self.as_json(
+      only: [:id, :name_english, :name_japanese, :name_romaji],
+      include: {
+        types: {
+          only: [:id, :english, :japanese]
+        }
+      }
+    )
+  end
+
+  Pokemon.__elasticsearch__.create_index!(force: true)
+  Pokemon.import
 
   def self.get_random_pokemon(quantity)
     Pokemon.find(Pokemon.pluck(:id).sample(quantity))
