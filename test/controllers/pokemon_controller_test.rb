e8a51431e1cbcae1049c0fcd3a07d784940b9612
Ajay <ajay.summer2@gmail.com>
2020-07-20 19:35:59 +0100

    Add routing related integration tests

--- test/controllers/pokemon_controller_test.rb
+++ test/controllers/pokemon_controller_test.rb
@@ -7,14 +7,36 @@ class PokemonControllerTest < ActionDispatch::IntegrationTest
     @type = types(:one)
   end
 
-  test "should get home" do
-    get pokemon_path
+  test "should get search" do
+    get '/en/search'
     assert_response :success
-    assert_equal(@pokemon.types[0], @type)
   end
 
   test "should get show" do
-    get pokemon_path + "/" + @pokemon.id.to_s
+    get "/en/pokemon/#{@pokemon.id}"
     assert_response :success
+  end
+
+  test "should load japanese locale" do
+    get '/ja/search'
+    assert_response :success
+  end
+
+  test "should redirect without locale" do
+    get '/search'
+    assert_response :redirect
+  end
+
+  test "should redirect incorrect location" do
+    get '/en/search/hello'
+    assert_response :redirect
+  end
+
+  test "pokemon type is correct" do
+    assert_equal(@pokemon.types[0], @type)
+  end
+
+  test "pokemon generation is correct" do
+    assert_equal(@pokemon.generation, @generation)
   end
 end
