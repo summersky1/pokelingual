7f7a28ed68cd4b67ef13504a6ddb3e25020c9208
Ajay <ajay.summer2@gmail.com>
2020-07-25 22:11:43 +0100

    Add Elasticsearch tests for Japanese searches

--- test/models/pokemon_test.rb
+++ test/models/pokemon_test.rb
@@ -33,6 +33,22 @@ class PokemonTest < ActiveSupport::TestCase
   test "search by pokemon types" do
     pokemon_list = Pokemon.search('water ice', 1)
     assert_equal(pokemon_list[0].types[0].english, 'Water')
+    assert_equal(pokemon_list[0].types[1].english, 'Ice')
+  end
+
+  test "search using name prefix (ja)" do
+    pokemon_list = Pokemon.search('ピカ', 1)
+    assert_equal(pokemon_list[0].id, '25')
+  end
+
+  test "search with slightly incorrect spelling (ja)" do
+    pokemon_list = Pokemon.search('ピカチウ', 1)
+    assert_equal(pokemon_list[0].id, '25')
+  end
+
+  test "search by pokemon types (ja)" do
+    pokemon_list = Pokemon.search('みず こおり', 1)
+    assert_equal(pokemon_list[0].types[0].japanese, 'みず')
     assert_equal(pokemon_list[0].types[1].japanese, 'こおり')
   end
 end
