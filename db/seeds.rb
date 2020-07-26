90a54fcae0158f32b776a1d77e8f78f4ff641cc4
Ajay <ajay.summer2@gmail.com>
2020-07-26 12:53:17 +0100

    Use activerecord-import to speed up data import time

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,52 @@
 require 'csv'
 
 Generation.delete_all
+generations = []
 CSV.foreach('lib/datasets/generations.csv', headers: true) do |row|
-  Generation.create({
+  generations << Generation.new({
     id: row[0],
     region_english: row[1],
     region_japanese: row[2]
   })
 end
+Generation.bulk_import(generations)
 
 Type.delete_all
+types = []
 CSV.foreach('lib/datasets/types.csv', headers: true) do |row|
-  Type.create({
+  types << Type.new({
     id: row[0],
     english: row[1],
     japanese: row[2],
   })
 end
+Type.bulk_import(types)
 
 PokemonType.delete_all
-
 Pokemon.delete_all
+pokemon_list = []
+pokemon_types = []
 CSV.foreach('lib/datasets/pokemon.csv', headers: true) do |row|
-  pokemon = Pokemon.create({
+  pokemon_list << Pokemon.new({
     id: row[0],
     name_english: row[1],
     name_japanese: row[2],
     name_romaji: row[3],
     generation_id: row[6]
   })
-  pokemon.types << (Type.find_by_english(row[4]))
-  pokemon.types << (Type.find_by_english(row[5])) if row[5].present?
+  pokemon_types << PokemonType.new({
+    pokemon_id: row[0],
+    type_id: (Type.find_by_english(row[4]).id)
+  })
+  if row[5].present?
+    pokemon_types << PokemonType.new({
+      pokemon_id: row[0],
+      type_id: (Type.find_by_english(row[5]).id)
+    })
+  end
 end
+Pokemon.bulk_import(pokemon_list)
+PokemonType.bulk_import(pokemon_types)
 
 previous_id = 0
 CSV.foreach('lib/datasets/pokemon_name_origins_jp.csv') do |row|
