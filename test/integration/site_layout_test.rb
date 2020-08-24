10b60c6358ba1fd0368830fc531056bb7203a264
Ajay <ajay.summer2@gmail.com>
2020-08-24 18:34:01 +0100

    Fix failing test due to moving to AJAX request

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -5,7 +5,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test 'navbar links' do
     get '/en/search'
     assert_template 'pokemon/search'
-    assert_select 'a[href=?]', '/en/search', count: 3
+    assert_select 'a[href=?]', '/en/search', count: 2
     assert_select 'a[href=?]', '/ja/search'
     (1..8).each do |gen|
       assert_select 'a[href=?]', "/en/generation/#{gen}"
